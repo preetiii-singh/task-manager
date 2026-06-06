9796d725ba1a7f4b20adb32b519536d131200f78
Preeti <work.preetisingh22@gmail.com>
2026-06-06 16:23:34 +0530

    Fix env loading for production

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
-
-dotenv.config()
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 const app = express()
 
